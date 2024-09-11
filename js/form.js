2f467c7d2f358c97f9d25b255ef7b8428e18b005
OLADIMEJI988 <oladimejimax49@gmail.com>
2024-09-11 17:02:52 +0100

    first commit

---
+++ js/form.js
@@ -0,0 +1,12 @@
+// const datePicker = document.getElementById('datePicker');
+
+// // Add event listener to detect change and display the date
+// datePicker.addEventListener('change', function() {
+//   if (datePicker.value) {
+//     datePicker.classList.remove('hide-text');
+//     datePicker.classList.add('show-text');
+//   } else {
+//     datePicker.classList.remove('show-text');
+//     datePicker.classList.add('hide-text');
+//   }
+// });
